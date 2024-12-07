133a605101dec6fe04aa67c9f76aebb6e26d0ab2
Alwi Djoenaid <alwi.djoenaid@gmail.com>
2024-12-07 15:54:39 +0700

    feat: implemented exports feature

---
+++ src/api/exports/handler.js
@@ -0,0 +1,34 @@
+class ExportsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postExportNotesHandler = this.postExportNotesHandler.bind(this);
+  }
+
+  async postExportNotesHandler(request, h) {
+    this._validator.validateExportNotesPayload(request.payload);
+
+    const message = {
+      userId: request.auth.credentials.id,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    // try {
+    //   await this._service.sendMessage('export:notes', JSON.stringify(message));
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    await this._service.sendMessage('export:notes', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda dalam antrean',
+    });
+
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
